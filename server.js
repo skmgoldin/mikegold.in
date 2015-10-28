541839f80bf941f498bd4680864efc6d5bb89e29
Mike Goldin <skmgoldin@gmail.com>
2015-10-28 19:54:24 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+var PORT = 80;
+var CONTENTPATH = 'html';
+
+var reqHandler = function(req, resp) {
+  var requrl = CONTENTPATH + req.url;
+  if(requrl == CONTENTPATH + '/') {
+    requrl = CONTENTPATH + '/index.html';
+  }
+
+  console.log(requrl);
+  fs.open(requrl, 'r', function(err, fd) {
+    if(err) {
+      resp.end('Sorry, couldn\'t fetch ' + requrl);
+      return;
+    }
+    fs.fstat(fd, function(err, stats) {
+      var contentBuf = new Buffer(stats.size);
+      fs.read(fd, contentBuf, 0, stats.size, 0, function(err, bytesRead, buf) {
+        resp.end(buf);
+        fs.close(fd, function(err) {
+          if(err)
+            console.log('Couldn\'t close ' + requrl);
+        });
+      });
+    });
+  });
+};
+
+var server = http.createServer(reqHandler);
+
+server.listen(PORT);
