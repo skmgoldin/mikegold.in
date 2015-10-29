f159a86bbe7e31d0738c901121e06c5939f142bf
Mike Goldin <skmgoldin@gmail.com>
2015-10-28 23:00:45 -0400

    Fetch and display last commit pushed to GitHub.

--- html/lastcommit.js
+++ html/lastcommit.js
@@ -1,7 +1,37 @@
+var lastCommitDateString;
+
+var header = {
+  "Accept": "application/vnd.github.v3+json"
+}
+
 var req = {
+  headers: header,
   success: function(data, textStatus, jqXHR) {
-    console.log(data);  
+    var foundCommit = false;
+    var i = 0;
+    var pushObj;
+    while(foundCommit != true && i < data.length) {
+      if(data[i].type == "PushEvent") {
+        foundCommit = true;
+        pushObj = data[i];
+      }
+      i++;
+    }
+
+    var commitsInPush = pushObj.payload.commits.length;
+    var commitUrl = pushObj.payload.commits[commitsInPush - 1].url;
+    jQuery.ajax(commitUrl, {
+      headers: header,
+      success: function(data, textStatus, jqXHR) {
+        lastCommitDateString = new Date(data.commit.author.date);
+        lastCommitDateString = lastCommitDateString.toLocaleDateString();
+        document.getElementById("lastcommit").innerHTML = lastCommitDateString;
+      }
+    });
   }
 };
 
-jQuery.ajax("http://api.github.com", req);
+jQuery(document).ready(function() {
+  jQuery.ajax("http://api.github.com/users/skmgoldin/events/public", req);
+});
+
