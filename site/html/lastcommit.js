b66bfb454101be1c034cf93a98db23b7157c8b28
Mike Goldin <mike.goldin@consensys.net>
2016-08-27 12:43:48 -0400

    Refactor lastcommit.js, display last commit hash
    Refactor for modularity, style and mitigation of callback hell. Display
    the most recent commit's hash rather than its date.

--- site/html/lastcommit.js
+++ site/html/lastcommit.js
@@ -1,39 +1,52 @@
-var lastCommitDateString;
-var oAuth = "?access_token=602efd22f1647b9a105dcdef4e674f34419cf288";
+var oAuth = "?access_token=602efd22f1647b9a105dcdef4e674f34419cf288"
 
 var header = {
   "Accept": "application/vnd.github.v3+json"
 }
 
+var writeLastCommitHTML = function(commit) {
+  lastCommitSHA = "0x" + commit.sha.slice(0, 8)
+  lastCommitURL = commit.html_url
+  lastCommitHTML = "<a href=\"" + lastCommitURL + "\">" + lastCommitSHA 
+                   + "</a>"
+
+  jQuery(document).ready(function() {
+    document.getElementById("lastcommit").innerHTML = lastCommitHTML
+  })
+}
+
+var getLastCommitObj = function(commitUrl, callback) {
+  jQuery.ajax(commitUrl + oAuth, {
+    headers: header,
+    success: function(data, textStatus, jqXHR) {
+      callback(data)
+    }
+  })
+}
+
+var findPushEvent = function(activity) {
+  var foundPush = false
+  var i = 0
+  var pushEvent = null
+  while(foundPush == false && i < activity.length) {
+    if(activity[i].type == "PushEvent") {
+      foundPush = true
+      pushEvent = activity[i]
+    }
+    i++
+  }
+  return pushEvent
+}
+
 var req = {
   headers: header,
   success: function(data, textStatus, jqXHR) {
-    var foundCommit = false;
-    var i = 0;
-    var pushObj;
-    while(foundCommit != true && i < data.length) {
-      if(data[i].type == "PushEvent") {
-        foundCommit = true;
-        pushObj = data[i];
-      }
-      i++;
-    }
-
-    var commitsInPush = pushObj.payload.commits.length;
-    var commitUrl = pushObj.payload.commits[commitsInPush - 1].url;
-    jQuery.ajax(commitUrl + oAuth, {
-      headers: header,
-      success: function(data, textStatus, jqXHR) {
-        lastCommitDateString = new Date(data.commit.author.date);
-        lastCommitDateString = lastCommitDateString.toLocaleDateString();
-
-        jQuery(document).ready(function() {
-          document.getElementById("lastcommit").innerHTML = lastCommitDateString;
-        });
-      }
-    });
+    var pushEvent = findPushEvent(data)
+    var commitsInPush = pushEvent.payload.commits.length
+    var commitUrl = pushEvent.payload.commits[commitsInPush - 1].url
+    getLastCommitObj(commitUrl, writeLastCommitHTML)
   }
-};
+}
 
-jQuery.ajax("https://api.github.com/users/skmgoldin/events" + oAuth, req);
+jQuery.ajax("https://api.github.com/users/skmgoldin/events" + oAuth, req)
 
