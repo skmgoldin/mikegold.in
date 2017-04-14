49ed1dcd70a88733892bbfeb8e6050bcfeb3aa07
Mike Goldin <mike.goldin@consensys.net>
2017-04-14 14:13:31 -0700

    Update paving script
    Docker installation is simpler than it once was.

--- devops/pave-ubuntu1604.sh
+++ devops/pave-ubuntu1604.sh
@@ -3,15 +3,12 @@
 # Install updates
 apt-get update && apt-get upgrade -y
 # Install Docker:
-apt-get install -y git python apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-rm /etc/apt/sources.list.d/docker.list
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+apt-get remove docker docker-engine
+apt-get install -y apt-transport-https ca-certificates curl \
+  software-properties-common git
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable"
 apt-get update
-apt-get purge lxc-docker
-apt-get install -y docker-engine python python-pip python-dev
-service docker start
-pip install --upgrade pip
-pip install docker-compose
-shutdown -r now
+apt-get install -y docker-ce
 
