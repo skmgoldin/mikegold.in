9eeac6e4ae96ba932fa855cd0f4cbbee790e6d04
Mike Goldin <mike.goldin@consensys.net>
2017-04-14 14:22:51 -0700

    Simplify structure, abstract out reverse proxy

--- nginx/getcerts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nohup nginx -c /init-nginx.conf &
-certbot certonly -n --agree-tos --email skmgoldin@gmail.com --webroot -w /usr/share/nginx/html -d hidden.computer -d www.hidden.computer
-nginx -s stop
-echo "@daily certbot renew" > /crontab.file
-crontab /crontab.file
-nginx -c /next-nginx.conf -g "daemon off;"
