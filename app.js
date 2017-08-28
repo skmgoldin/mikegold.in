e8e182d2e38748253d69459f2340cf2ba747676f
Mike Goldin <mike.goldin@consensys.net>
2017-08-27 20:42:00 -0400

    Force https

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(function(req, res, next) {
+      if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+                res.redirect('https://' + req.get('Host') + req.url);
+            }
+      else
+            next();
+});
 app.use(express.static('public'))
 
 app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:' + port + '/');
 })
+
 
