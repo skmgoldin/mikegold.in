995ee33616e1e3bf26520b0afa1b66947c8efb3e
Mike Goldin <mike.goldin@consensys.net>
2018-04-01 19:41:12 -0400

    Run the linter on existing Javascript
    The Javascript in this repo has never been linted before.
    
    * Run the linter on the repo's Javascript

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-const port = process.env.PORT || 3000
+const app = express();
 
-app.use(function(req, res, next) {
-      if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-                res.redirect('https://' + req.get('Host') + req.url);
-            }
-      else
-            next();
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  if ((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect(`https://${req.get('Host')}${req.url}`);
+  } else { next(); }
 });
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(port, function () {
-  console.log('Server running at http://127.0.0.1:' + port + '/');
-})
-
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
 
--- public/js/lastcommit.js
+++ public/js/lastcommit.js
@@ -1,52 +1,52 @@
-var oAuth = "?access_token=602efd22f1647b9a105dcdef4e674f34419cf288"
+const oAuth = '?access_token=602efd22f1647b9a105dcdef4e674f34419cf288';
 
-var header = {
-  "Accept": "application/vnd.github.v3+json"
-}
+const header = {
+  Accept: 'application/vnd.github.v3+json',
+};
 
-var writeLastCommitHTML = function(commit) {
-  lastCommitSHA = "0x" + commit.sha.slice(0, 8)
-  lastCommitURL = commit.html_url
-  lastCommitHTML = "<a href=\"" + lastCommitURL + "\">" + lastCommitSHA 
-                   + "</a>"
+const writeLastCommitHTML = function (commit) {
+  lastCommitSHA = `0x${commit.sha.slice(0, 8)}`;
+  lastCommitURL = commit.html_url;
+  lastCommitHTML = `<a href="${lastCommitURL}">${lastCommitSHA
+  }</a>`;
 
-  jQuery(document).ready(function() {
-    document.getElementById("lastcommit").innerHTML = lastCommitHTML
-  })
-}
+  jQuery(document).ready(() => {
+    document.getElementById('lastcommit').innerHTML = lastCommitHTML;
+  });
+};
 
-var getLastCommitObj = function(commitUrl, callback) {
+const getLastCommitObj = function (commitUrl, callback) {
   jQuery.ajax(commitUrl + oAuth, {
     headers: header,
-    success: function(data, textStatus, jqXHR) {
-      callback(data)
-    }
-  })
-}
+    success(data, textStatus, jqXHR) {
+      callback(data);
+    },
+  });
+};
 
-var findPushEvent = function(activity) {
-  var foundPush = false
-  var i = 0
-  var pushEvent = null
-  while(foundPush == false && i < activity.length) {
-    if(activity[i].type == "PushEvent") {
-      foundPush = true
-      pushEvent = activity[i]
+const findPushEvent = function (activity) {
+  let foundPush = false;
+  let i = 0;
+  let pushEvent = null;
+  while (foundPush == false && i < activity.length) {
+    if (activity[i].type == 'PushEvent') {
+      foundPush = true;
+      pushEvent = activity[i];
     }
-    i++
+    i++;
   }
-  return pushEvent
-}
+  return pushEvent;
+};
 
-var req = {
+const req = {
   headers: header,
-  success: function(data, textStatus, jqXHR) {
-    var pushEvent = findPushEvent(data)
-    var commitsInPush = pushEvent.payload.commits.length
-    var commitUrl = pushEvent.payload.commits[commitsInPush - 1].url
-    getLastCommitObj(commitUrl, writeLastCommitHTML)
-  }
-}
+  success(data, textStatus, jqXHR) {
+    const pushEvent = findPushEvent(data);
+    const commitsInPush = pushEvent.payload.commits.length;
+    const commitUrl = pushEvent.payload.commits[commitsInPush - 1].url;
+    getLastCommitObj(commitUrl, writeLastCommitHTML);
+  },
+};
 
-jQuery.ajax("https://api.github.com/users/skmgoldin/events" + oAuth, req)
+jQuery.ajax(`https://api.github.com/users/skmgoldin/events${oAuth}`, req);
 
